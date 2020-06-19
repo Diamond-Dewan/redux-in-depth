404721a63cdf5d8a8ce1d04550fe005a017887ef
Diamond-Dewan <diamonddewan05@gmail.com>
2020-06-20 04:27:56 +0600

    implement middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
+import logger from './middleware/logger';
 
 // create
 export default function () {
-  return configureStore({ reducer: reducer });
+  return configureStore({
+    reducer: reducer,
+    middleware: [logger],
+  });
 }
